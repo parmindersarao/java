0fb5804994b9e65636aea8fdb2bec8a401bdb9bf
parmindersarao <psrandomtalk@gmail.com>
2021-12-15 15:22:26 +0530

    practice

--- test.java
+++ test.java
@@ -10,23 +10,62 @@
     }
     
 
-}*/
+}
+// filename Main.java
+class Grandparent {
+   
+	public void Print()
+	{
+		System.out.println("Grandparent's Print()" + a);
+	}
+} */
+
+class Parent{
+    int a;
+    Parent(int x){
+        a=x;
+    }
+    void Print()
+	{
+		System.out.println("Parent's Print()" +a );
+	}
+}
+
+class Child extends Parent {
+	
+    Child(int x){
+        super(x);
+    }
+    @Override
+    void Print(){
+        super.Print();
+        System.out.println("Child's Print()"+a);
+    }
+}
+    
 
 
 class test {
-    static int a;
-    static void get(int x){
-        a=x;
+      /*  int a;
+    char b;
+    void get(char x, int y){
+        b=x;
+        a=y;
     }
+    void show(){
+        System.out.println(b);
+        System.out.println(a);
+    }*/
     public static void main(String[] args) {
-        //test obj1 = new test();
-       // obj1.get(5);
-        //obj1.show();
-        get(5);
-        show();
+        Parent P = new Parent(8);
+        Child C = new Child(10);
+       /* C.Print(); // calling function through child class object it print 10 
+        C.show(); // same but it is child class function
+        P.Print(); // callin function by parent class which print 8 because we pass value 8
+        P=C; // here we assigning reference to parent class 
+        P.Print(); // here we call again parent class functin but after referencing so it is gonna print 10 */
+        C.Print();
 
     }
-    static void show(){
-        System.out.println(a);
-    }
+
 }
